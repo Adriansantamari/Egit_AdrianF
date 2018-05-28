73616a596ffba8c873bbb0ef452bd9a5ecc1a37b
adrian <adrian@DESKTOP-NK0VERE.Home>
2018-05-28 16:26:54 +0200

    Esto es un cambio.

--- GitHub_Practica/src/Ejemplo.java
+++ GitHub_Practica/src/Ejemplo.java
@@ -4,7 +4,7 @@ public class Ejemplo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("Hola, soy Adrian y hago el proyecto con Josu");
-
+		 System.out.println("Hola, son las 11:00");
 	}
 
 }
